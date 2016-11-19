fbc962e9716f0cc73316d4c54a9ebdf77246949e
karina <you@example.com>
2016-11-19 15:38:49 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,21 @@
+print("Hello, Django Girls!")
+print("karina")
+name = "karina"
+name1 = "sarah"
+print("name"+"name1")
+if 3 > 2: print("It works!")
+
+if 3<1: print("WROONG!!")
+if 3 > 1: print("OK!!!")
+if 5 > 2:
+    print('5 is indeed greater than 2')
+    name = "sonja"
+    if name == "sonja" : print("Hey Sonja!")
+    if 3>1: print("3 is greater than 1")
+else: print ("3 is not greater than 1")
+if 3>4: print("3 is greater than 4")
+else: print ("3 is not greater than 4")
+name = "sonja"
+if name == "sonja": print ("hey sonja")
+if name == "sonja": print ("what a nice name!")
+elif name == 'Sonja':print('Hey Sonja!')
